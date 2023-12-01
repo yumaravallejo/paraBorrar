4eb3dbea6b3bfdc5783de1b1a2fabb640ca42436
yvalval1411 <yvalval1411@g.educaand.es>
2023-12-01 09:03:44 +0100

    Llueve

--- src/main/java/ejemplo/ParaBorrar.java
+++ src/main/java/ejemplo/ParaBorrar.java
@@ -11,6 +11,6 @@ package ejemplo;
 public class ParaBorrar {
 
     public static void main(String[] args) {
-        System.out.println("Eva se aburre");
+        System.out.println("Está lloviendo");
     }
 }

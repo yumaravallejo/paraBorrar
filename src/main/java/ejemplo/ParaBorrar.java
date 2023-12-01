0349322a57d931a7f60b9a6971febdf101259747
yvalval1411 <yvalval1411@g.educaand.es>
2023-12-01 08:56:01 +0100

    Inicio de proyecto

---
+++ src/main/java/ejemplo/ParaBorrar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejemplo;
+
+/**
+ *
+ * @author yumara
+ */
+public class ParaBorrar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
